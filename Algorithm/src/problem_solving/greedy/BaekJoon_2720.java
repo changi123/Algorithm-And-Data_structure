a59dc3771f9f949d75157b1ef2896f2b41087d3d
hcghcg17 <hcghcg17@naver.com>
2024-03-12 23:38:47 +0900

    Feat : BaekJoon_2720

--- Algorithm/src/problem_solving/greedy/BaekJoon_2720.java
+++ Algorithm/src/problem_solving/greedy/BaekJoon_2720.java
@@ -2,21 +2,55 @@ package problem_solving.greedy;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class BaekJoon_2720 {
 
 	public static void main(String[] args) {
-//		String number = "4177252841";
-		String number = "1924";
+		//		쿼터(Quarter, $0.25)의 개수, 
+		//		다임(Dime, $0.10)의 개수,
+		//		니켈(Nickel, $0.05)의 개수,
+		//		페니(Penny, $0.01)의 개수를 구하는 프로그램을 작성하시오.
+		//		거스름돈은 항상 $5.00 이하이고, 손님이 받는 동전의 개수를 최소로 하려고 한다.
+		//		예를 들어, $1.24를 거슬러 주어야 한다면, 손님은 4쿼터, 2다임, 0니켈, 4페니를 받게 된다.
 		
-		int k = 2 ;
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println(solution(number, k));
+		int T = Integer.parseInt(sc.next());
+	
+//		int cnt = s / 25;
+//		s = s % 25 ;
+//		cnt = s / 10 ;
+//		s = s % 10;
+//		cnt = s / 1 ;
+//		s = s % 1;
+		int cnt = 0 ;
+		for(int i = 0; i < T ; i++) {
+			int caseNum = Integer.parseInt(sc.next());
+			cnt = caseNum / 25 ;
+			caseNum = caseNum%25;
+			System.out.print(cnt);
+			System.out.print(" ");
+			cnt = caseNum / 10 ;
+			caseNum = caseNum % 10;
+			System.out.print(cnt);
+			System.out.print(" ");
+			cnt = caseNum / 5 ;
+			caseNum = caseNum % 5;
+			System.out.print(cnt);
+			System.out.print(" ");
+			cnt = caseNum / 1 ;
+			caseNum = caseNum % 1;
+			System.out.println(cnt);
+			
+		}
+		
+		
+		
+		
+		
+		
 
 	}
-    public static String solution(String number, int k) {
-        String answer = "";
-        
-        return answer;
-    }
+
 }
