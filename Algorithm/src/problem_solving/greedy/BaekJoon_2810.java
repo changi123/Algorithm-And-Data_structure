7af03b06653c8769f671fe923750e3298eb04c86
hcghcg17 <hcghcg17@naver.com>
2024-03-13 22:51:19 +0900

    Feat : BaekJoon2810 완료

---
+++ Algorithm/src/problem_solving/greedy/BaekJoon_2810.java
@@ -0,0 +1,33 @@
+package problem_solving.greedy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJoon_2810 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N = Integer.parseInt(sc.next());
+		String s = sc.next();
+		
+		int len = s.length();
+		s = s.replace("LL", "S");
+		int cnt = 0;
+		if( len == s.length()) {
+			for(int i= 0 ; i <s.length();i++) {
+				cnt++;
+			}
+			System.out.println(cnt);
+		} else {
+			
+			for(int i= 0 ; i <s.length();i++) {
+				cnt++;
+			}
+			System.out.println(cnt+1);
+		}
+		
+	}
+
+}
