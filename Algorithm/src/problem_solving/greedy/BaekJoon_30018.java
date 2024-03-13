d567a24c862e5f19508c7fa49bdc37fc2d0243dc
hcghcg17 <hcghcg17@naver.com>
2024-03-13 19:50:30 +0900

    Feat : BaekJoon_30018 완료

---
+++ Algorithm/src/problem_solving/greedy/BaekJoon_30018.java
@@ -0,0 +1,37 @@
+package problem_solving.greedy;
+
+import java.util.Scanner;
+
+public class BaekJoon_30018 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = Integer.parseInt(sc.next()); //N개 대여소
+		
+		
+		int [] aArr = new int[N];
+		int [] bArr = new int[N];
+		
+		
+		for(int i= 0 ; i < N ; i++) {
+			aArr[i] = Integer.parseInt(sc.next());
+		}
+		
+		for(int i= 0 ; i < N ; i++) {
+			bArr[i] = Integer.parseInt(sc.next());
+		}
+		
+		int cnt = 0;
+		for(int i =0  ; i <N ; i++) {
+			int num = bArr[i] - aArr[i] ;
+			if( num > 0) {
+				cnt = cnt + num;
+			}
+		}
+		
+		System.out.println(cnt);
+		
+		
+	}
+
+}
