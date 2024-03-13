dcf7611d643a0ab5b990b2ce90ede616cb78ba9a
hcghcg17 <hcghcg17@naver.com>
2024-03-13 21:25:09 +0900

    Feat : BaekJoon_4796 완료

---
+++ Algorithm/src/problem_solving/greedy/BaekJoon_4796.java
@@ -0,0 +1,37 @@
+package problem_solving.greedy;
+
+import java.util.Scanner;
+
+public class BaekJoon_4796 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int i = 1 ;
+	    while(true) {
+	    	int L = Integer.parseInt(sc.next());
+	    	int P = Integer.parseInt(sc.next());
+	    	int V = Integer.parseInt(sc.next());
+	    	
+	    	
+	    	if( L == 0 && P == 0 && V == 0) {
+	    		break;
+	    	}
+	    	
+	    	
+	    	int a = (V/P) *L;
+	    	if( V % P >L) {
+	    		System.out.println("Case "+i+": "+ ( a+L));
+	    	}else {
+	    		System.out.println("Case "+i+": "+ ( a + V%P));
+	    		
+	    	}
+	    	
+	    	i++;
+	    	
+	    }
+	    
+	}
+
+}
