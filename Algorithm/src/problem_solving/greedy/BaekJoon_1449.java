58dd8a658180d031c7ea9c291bfd94f4bad3f6a4
hcghcg17 <hcghcg17@naver.com>
2024-03-15 10:45:56 +0900

    Feat : BaekJoon_1449 완료

---
+++ Algorithm/src/problem_solving/greedy/BaekJoon_1449.java
@@ -0,0 +1,45 @@
+package problem_solving.greedy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJoon_1449 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = Integer.parseInt(sc.next()); // 물이 새는 개수
+		int L = Integer.parseInt(sc.next()); // 테이프 길이
+
+		int [] arr = new int[N];
+
+		int cnt = 0 ;
+
+		for(int i= 0 ; i <arr.length ; i++) {
+			arr[i] = Integer.parseInt(sc.next());
+		}
+
+		Arrays.sort(arr);
+		int start = arr[0];
+		
+		
+		for(int i= 0 ; i< arr.length ; i++) {
+			if( arr[i] - start >= L) {
+				cnt++;
+				start = arr[i];
+			}
+			
+		}
+		
+		System.out.println(cnt+1);
+
+
+
+
+
+
+
+	}
+
+}
