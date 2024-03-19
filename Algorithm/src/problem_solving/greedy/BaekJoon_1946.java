dbbee10f9b53cefd2d8fdaa5160df1956b111d11
hcghcg17 <hcghcg17@naver.com>
2024-03-20 00:21:53 +0900

    Feat : BaekJoon_1946 완료

---
+++ Algorithm/src/problem_solving/greedy/BaekJoon_1946.java
@@ -0,0 +1,53 @@
+package problem_solving.greedy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class BaekJoon_1946 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int T = Integer.parseInt(sc.next()); // 테스크 케이스 수
+
+		for(int i = 0 ; i < T ; i++) {
+
+			int N = Integer.parseInt(sc.next()); // 지원자 수
+			int [][] arr = new int [N][2];
+			for(int j= 0 ; j < N ; j++) {				
+				int grade = Integer.parseInt(sc.next()); // 성적
+				int rank = Integer.parseInt(sc.next()); // 순위
+
+				arr[j][0] = grade;
+				arr[j][1] = rank;
+
+
+			}
+
+			Arrays.sort(arr, new Comparator<int[]>() {
+				@Override
+				public int compare(int[] o1, int[] o2) {
+					return Integer.compare(o1[0], o2[0]);
+				}
+
+			});
+
+
+			int cnt = 0;
+			int max = arr[0][1];
+			for(int j = 0 ; j <N ; j++) {
+					if( max<arr[j][1]) {
+						cnt++;
+						continue;
+					}
+					max = arr[j][1];
+
+			}
+			System.out.println(N-cnt);
+		}
+
+	}
+
+}
