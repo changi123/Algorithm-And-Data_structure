33f34014b2c530e75e8461e2504c7b511b32dcad
hcghcg17 <hcghcg17@naver.com>
2024-03-20 12:33:09 +0900

    Feat : BaekJoon_2742 완료

---
+++ Algorithm/src/problem_solving/implement/BaekJoon_2742.java
@@ -0,0 +1,20 @@
+package problem_solving.implement;
+
+import java.util.Scanner;
+
+public class BaekJoon_2742 {
+
+	public static void main(String[] args) {
+
+			
+		Scanner sc = new Scanner(System.in);
+		
+		int n = Integer.parseInt(sc.next());
+		
+		
+		for(int i = n ; i>=1 ; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
