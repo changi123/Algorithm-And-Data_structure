97677b002429cf45a7eaa9f2b942eda88c2eba0c
hcghcg17 <hcghcg17@naver.com>
2024-03-20 21:12:13 +0900

    Feat : BaekJoon_2441 완료

---
+++ Algorithm/src/problem_solving/implement/BaekJoon_2441.java
@@ -0,0 +1,26 @@
+package problem_solving.implement;
+
+import java.util.Scanner;
+
+public class BaekJoon_2441 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		
+		for(int i = 0 ; i<n ; i++) {
+			
+			for(int j = 0 ; j<n;j++) {
+				if( j < i) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
