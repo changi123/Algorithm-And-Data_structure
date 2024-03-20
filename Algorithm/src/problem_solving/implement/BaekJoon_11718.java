c4183eebb455a2064a7ab36ff2380bbde7957375
hcghcg17 <hcghcg17@naver.com>
2024-03-20 15:07:12 +0900

    Feat : BaekJoon_11718 완료

---
+++ Algorithm/src/problem_solving/implement/BaekJoon_11718.java
@@ -0,0 +1,16 @@
+package problem_solving.implement;
+
+import java.util.Scanner;
+
+public class BaekJoon_11718 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine()) {
+			String s = sc.nextLine();
+			System.out.println(s);
+		}
+	}
+
+}
