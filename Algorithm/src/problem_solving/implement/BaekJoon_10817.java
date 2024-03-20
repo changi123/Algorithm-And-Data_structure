7bd409b4c6a8b5098654d96d54f2af0717705f63
hcghcg17 <hcghcg17@naver.com>
2024-03-20 15:50:12 +0900

    Feat : BaekJoon_10817 완료

---
+++ Algorithm/src/problem_solving/implement/BaekJoon_10817.java
@@ -0,0 +1,37 @@
+package problem_solving.implement;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Scanner;
+
+public class BaekJoon_10817 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int [] arr = new int[3];
+		for(int i= 0 ; i < arr.length;i++) {			
+			arr[i]=  Integer.parseInt(sc.next());
+		}
+		
+		
+		Arrays.sort(arr);
+		
+		
+
+
+		System.out.println(arr[1]);
+
+
+
+
+
+
+
+
+	}
+
+}
