02b8bf4203cbac4dcc9a77fe8ce007cd66c4a29d
hcghcg17 <hcghcg17@naver.com>
2024-03-22 14:06:47 +0900

    Feat : BaekJoon_2750 완료

---
+++ Algorithm/src/problem_solving/implement/BaekJoon_2750.java
@@ -0,0 +1,38 @@
+package problem_solving.implement;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BaekJoon_2750 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = Integer.parseInt(sc.next());
+		
+		
+		
+		ArrayList<Integer> list = new ArrayList();
+		
+		
+		for(int i= 0 ; i < n; i ++) {
+			String s = sc.next();
+			if( !list.contains(s)) {
+				list.add(Integer.parseInt(s));
+			}
+		}
+		
+	
+//		System.out.println(list.toString());
+		
+		list.sort(null);
+		
+		for(int i= 0 ; i <list.size(); i ++) {
+			System.out.println(list.get(i));
+		}
+		
+		
+	}
+
+}
