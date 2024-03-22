b29278043b85565e49d056e5e068f55028d1063c
hcghcg17 <hcghcg17@naver.com>
2024-03-22 14:24:29 +0900

    Feat : BaekJonn_5622 완료

---
+++ Algorithm/src/problem_solving/implement/BaekJoon_5622.java
@@ -0,0 +1,35 @@
+package problem_solving.implement;
+
+import java.util.Scanner;
+
+public class BaekJoon_5622 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String [] arr = sc.next().split("");
+		
+		String [] d = {"0","0","0","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};
+		
+		
+		int sum = 0 ;
+		for(int i = 0 ; i < arr.length ; i++) {
+			
+			String s = arr[i];
+			
+			for(int j = 0 ; j < d.length;j++) {
+				
+				if( d[j].contains(s)) {
+					sum+=j;
+					break;
+				}
+				
+			}
+			
+		}
+		
+		System.out.println(sum);
+	}
+
+}
