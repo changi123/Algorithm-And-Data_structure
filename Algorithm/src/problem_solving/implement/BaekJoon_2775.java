053b030ab53586d4659df1b022bc0cdcc6aea93f
hcghcg17 <hcghcg17@naver.com>
2024-03-22 14:56:17 +0900

    Feat : BaekJoon_2775 완료

---
+++ Algorithm/src/problem_solving/implement/BaekJoon_2775.java
@@ -0,0 +1,59 @@
+package problem_solving.implement;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJoon_2775 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+
+
+
+		int T = Integer.parseInt(sc.next());
+
+		while(T >0) {
+
+			int n = Integer.parseInt(sc.next());
+			int m = Integer.parseInt(sc.next());
+
+
+			int [][] arr = new int[n+1][m];
+
+
+			//		System.out.println(Arrays.deepToString(arr));
+
+			int answer = 0;
+			for(int i= 0 ; i < arr.length; i++) {
+				for(int j = 0 ;  j< arr[i].length; j++) {
+					if( i == 0) {
+						arr[i][j] = j+1 ;
+					} else {
+						int sum = 0 ; 
+						for(int k = 0 ; k <=j ; k++) {
+							sum+=arr[i-1][k];
+						}
+						arr[i][j] = sum;
+						if( j == m-1) {
+							answer= arr[i][j];
+							break;
+						}
+					}
+				}
+				if( i == n) {
+					System.out.println(answer);
+					break;
+				}
+			}
+
+
+			T--;
+		}
+
+
+
+	}
+
+}
