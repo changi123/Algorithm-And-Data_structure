a73e5e4b80933c8da4c87d2618a0d42a82dc109f
hcghcg17 <hcghcg17@naver.com>
2024-03-26 20:38:11 +0900

    Feat : BaekJoon_5576 완료

---
+++ Algorithm/src/problem_solving/sort/BackJoon_5576.java
@@ -0,0 +1,30 @@
+package problem_solving.sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BackJoon_5576 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int [] wArr = new int[10];
+		int [] kArr = new int[10];
+		int w = 0 ;
+		int k = 0 ;
+		for(int i= 0 ; i  < 20 ; i++) {
+			if( i <10 ) {
+				wArr[w++] = Integer.parseInt(sc.next());
+			} else {
+				kArr[k++]= Integer.parseInt(sc.next());
+			}
+		}
+		
+		
+		Arrays.sort(wArr);
+		Arrays.sort(kArr);
+		
+		System.out.print(wArr[wArr.length-1]+wArr[wArr.length-2]+wArr[wArr.length-3] + " ");
+		System.out.print(kArr[kArr.length-1]+kArr[kArr.length-2]+kArr[kArr.length-3] + " ");
+	}
+
+}
