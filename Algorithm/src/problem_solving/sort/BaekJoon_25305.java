d3577d3c8e9f81112be37d979de932761594a7e3
hcghcg17 <hcghcg17@naver.com>
2024-03-26 13:30:29 +0900

    Feat  : BaekJoon_25305 완료

---
+++ Algorithm/src/problem_solving/sort/BaekJoon_25305.java
@@ -0,0 +1,24 @@
+package problem_solving.sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJoon_25305 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		int k = Integer.parseInt(sc.next());
+		int [] arr = new int[n];
+		
+		for(int i =  0 ; i < n; i ++) {
+			arr[i] = Integer.parseInt(sc.next());
+		}
+		Arrays.sort(arr);
+		
+		
+		System.out.println(arr[arr.length-k]);
+	}
+
+}
