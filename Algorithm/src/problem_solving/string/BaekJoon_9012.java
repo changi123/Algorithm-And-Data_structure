3a58207cc0352028f9e981b358fac686ef536192
hcghcg17 <hcghcg17@naver.com>
2024-03-26 11:43:35 +0900

    Feat : BaekJoon_9012 완료

---
+++ Algorithm/src/problem_solving/string/BaekJoon_9012.java
@@ -0,0 +1,50 @@
+package problem_solving.string;
+
+import java.util.Scanner;
+
+public class BaekJoon_9012 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = Integer.parseInt(sc.next());
+
+		
+
+
+		int j = 1 ;
+		while(n > 0) {
+			String s = sc.next();
+
+			int cnt1 = 0;
+			int cnt2 = 0 ;
+			for(int i= 0 ; i <s.length() ;i++) {
+				
+				if( s.charAt(i) == '(') {
+					cnt1++;
+				}else {
+					cnt2++;
+				}
+				if( cnt2 > cnt1 ) {
+					System.out.println("NO");
+					break;
+				}
+				
+				if( cnt1 == cnt2 && i == s.length()-1) {
+					System.out.println("YES");
+					break;
+				} else if( s.charAt(s.length()-1) == '(') {
+					System.out.println("NO");
+					break;
+				} else if( cnt1 != cnt2 && s.length()-1 ==i) {
+					System.out.println("NO");
+					
+				}
+			}
+			
+			n--;
+			
+		}
+	}
+
+}
