9b260615df9c32d5e69e3482141566f200d83515
hcghcg17 <hcghcg17@naver.com>
2024-04-02 17:16:23 +0900

    Feat : BaekJoon_9996 완료

---
+++ Algorithm/src/problem_solving/string/BaekJoon_9996.java
@@ -0,0 +1,40 @@
+package problem_solving.string;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class BaekJoon_9996 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		
+		String s = sc.next();
+//		s = s .replace("*", "-");
+		String [] arr = s.split("\\*");
+		
+		while(n>0) {
+			String sr = sc.next();
+			
+			if( sr.length()  <arr[0].length()+arr[1].length()) {
+				n--;
+				System.out.println("NE");
+				continue;
+			}
+		
+			if( s.substring(0, arr[0].length()).equals(sr.substring(0,arr[0].length())) &&
+					
+					s.substring(s.length()-arr[1].length() ).equals(sr.substring(sr.length()-arr[1].length()))
+					) {
+				System.out.println("DA");
+			}else {
+				System.out.println("NE");
+			}
+			
+			n--;
+		}
+		
+	}
+
+}
