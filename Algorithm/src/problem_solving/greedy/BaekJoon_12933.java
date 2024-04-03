923c3601b48c6deacf5e1d7ab9e52c048da70997
hcghcg17 <hcghcg17@naver.com>
2024-04-03 21:19:26 +0900

    Feat : BaekJoon_ 2872 수정중

--- Algorithm/src/problem_solving/greedy/BaekJoon_12933.java
+++ Algorithm/src/problem_solving/greedy/BaekJoon_12933.java
@@ -8,11 +8,60 @@ public class BaekJoon_12933 {
 	public static void main(String[] args) {
 
 		Scanner sc= new Scanner(System.in);
-		String [] sr = sc.next().split("");
-		
-		
-		Arrays.sort(sr);
-		System.out.println(Arrays.toString(sr));
+		String s = sc.next();
+		int cnt = 0 ;
+		//q','u','a','c','k
+		while(true) {
+			StringBuilder sb= new StringBuilder();
+			
+			int len = s.length() ;
+			
+			
+			
+			int index1 =s.indexOf("q");
+			if( index1 <  0 ) {
+				break;
+			}
+			sb.append(s.charAt(index1));
+			s = s.replaceFirst("q", "_");
+			
+			int index2 =s.indexOf("u");
+			if( index2 <  0 || index2 < index1 ) {
+				break;
+			}
+			sb.append(s.charAt(index2));
+			s = s.replaceFirst("u", "_");
+			
+			int index3 =s.indexOf("a");
+			if( index3 <  0 || index3 < index2) {
+				break;
+			}
+			sb.append(s.charAt(index3));
+			s = s.replaceFirst("a", "_");
+			
+			int index4 =s.indexOf("c");
+			if( index4 <  0 || index4 < index3) {
+				break;
+			}
+			sb.append(s.charAt(index4));
+			s = s.replaceFirst("c", "_");
+			
+			
+			int index5 =s.indexOf("k");
+			if( index5 <  0 || index5 < index4) {
+				break;
+			}
+			sb.append(s.charAt(index5));
+			s = s.replaceFirst("k", "_");
+			
+			if( sb.toString().equals("quack")) {
+				cnt++;
+				
+			}
+			
+			
+		}
+		System.out.println(cnt);
 		
 	}
 
---
+++ Algorithm/src/problem_solving/greedy/BaekJoon_2872.java
@@ -0,0 +1,23 @@
+package problem_solving.greedy;
+
+import java.util.Scanner;
+
+public class BaekJoon_2872 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = Integer.parseInt(sc.next());
+		
+		int[] arr = new int[n];
+		
+		for(int i= 0 ; i < arr.length ; i++) {
+			arr[i] = Integer.parseInt(sc.next());
+		}
+		
+		
+		
+		
+	}
+
+}
