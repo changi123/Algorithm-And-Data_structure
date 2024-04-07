496208f1fc35017393a44992532ae7f15c2c64e9
hcghcg17 <hcghcg17@naver.com>
2024-04-07 12:05:53 +0900

    Feat : BaekJoon_17413 완료

--- Algorithm/src/problem_solving/string/BaekJoon_17413.java
+++ Algorithm/src/problem_solving/string/BaekJoon_17413.java
@@ -8,23 +8,53 @@ public class BaekJoon_17413 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
-		StringBuilder sb = new StringBuilder();
 		Stack<Character> st = new Stack();
+		StringBuilder sb = new StringBuilder();
+		boolean sbFlag = false;
+
+		char c ='a';
 		for(int i= 0 ; i < s.length(); i++) {
-			
 			if( s.charAt(i) == '<') {
-				st.push(s.charAt(i));
-			} else if( s.charAt(i) == '>') {
 				while(!st.isEmpty()) {
-					sb.append(st.pop());
+					System.out.print(st.pop());
 				}
-				sb.append(">");
-			} else {
+
+				sbFlag = true;
+				sb.append(s.charAt(i));
+				continue;
+			}else if( s.charAt(i) == '>' && sbFlag ==true) {
+				sb.append(s.charAt(i));
+				System.out.print(sb.toString());
+				sb.delete(0,sb.length());
+				sbFlag = false;
+				continue;
+			} 
+
+			if( sbFlag ) {
+				sb.append(s.charAt(i));
+			} else{
+
 				st.push(s.charAt(i));
-				
-			}
-			
+				if( s.charAt(i) == ' ' || i == s.length()-1) {
+					while(!st.isEmpty()) {
+						if(st.peek() == ' ') {
+							st.pop();
+							continue;
+						}
+						System.out.print(st.pop());
+					}
+					System.out.print(' ');
+
+				}
+
+			} 
+
+
+
 		}
+
 	}
+
+
 
 }
