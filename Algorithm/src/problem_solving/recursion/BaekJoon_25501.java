0639e508d06d7ae299d535db84a263db16cb49e9
hcghcg17 <hcghcg17@naver.com>
2024-04-08 13:04:00 +0900

    Feat : BaekJoon_25501 완료

---
+++ Algorithm/src/problem_solving/recursion/BaekJoon_25501.java
@@ -0,0 +1,29 @@
+package problem_solving.recursion;
+
+import java.util.Scanner;
+
+public class BaekJoon_25501 {
+
+	static int [][] arr ;
+	static int cnt  ; 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n= Integer.parseInt(sc.next());
+		arr = new int[n][1];
+		for(int i= 0 ; i < n ; i++) {
+			cnt = 0 ;
+			String s=  sc.next();
+			System.out.println(isPalindrome(s)+" "+ cnt);
+		}
+	}
+    public static int recursion(String s, int l, int r){
+    	cnt++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    public static int isPalindrome(String s){
+    	
+        return recursion(s, 0, s.length()-1);
+    }
+}
