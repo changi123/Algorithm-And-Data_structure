a23c2a6b49d2896667685682eeecdfa7be96d5b2
hcghcg17 <hcghcg17@naver.com>
2024-04-09 17:13:10 +0900

    Feat : 그래프+트리 정리

---
+++ Algorithm/src/data_structure/Graph.java
@@ -0,0 +1,22 @@
+package data_structure;
+
+public class Graph {
+
+	public static void main(String[] args) {
+		/*
+		 	그래프(Graph)
+		 	(1) 그래프는 단순히 nodes/vertices(노드) 사이에 edge가 있는 Collection이다
+		 	(2) 그래프는 directed(방향) 또는 undirected(무방향)이 될 수 있다.
+		 	
+		 	
+		 	트리(Tree)
+		 	(1) 노드들로 구성되어 있다
+		 	(2) 트리 최상단에 있는 노드를 root라고 한다
+		 	(3) 루트를 기준으로 접근하기 위한 거리를 depth 하고 한다
+		 	(4) 같은 부모를 가지면서 같은 깊이에 있는 노들들은 sibling 관계에 있다
+		 	(5) 노드와 노드를 잇는 선을 edge라고 한다
+		 	(6) 자식이 없는 노드는 leaf 라고 한다
+		 */
+	}
+
+}
