e43bed1474f4845706f2fa9fba83d10de9e9a8c4
hcghcg17 <hcghcg17@naver.com>
2024-04-11 15:18:37 +0900

    Feat : BaekJoon_9086 완료

---
+++ Algorithm/src/problem_solving/string/BaekJoon_9086.java
@@ -0,0 +1,27 @@
+package problem_solving.string;
+
+import java.util.Scanner;
+
+public class BaekJoon_9086 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = Integer.parseInt(sc.next());
+		
+		for(int i= 0 ; i < n ; i++) {
+			String s = sc.next();
+			if( s.length() > 1) {
+				System.out.print(s.charAt(0));
+				System.out.print(s.charAt(s.length()-1));
+				System.out.println();
+			}else {
+				System.out.print(s.charAt(0));
+				System.out.print(s.charAt(0));
+				System.out.println();
+			}
+		}
+
+	}
+
+}
