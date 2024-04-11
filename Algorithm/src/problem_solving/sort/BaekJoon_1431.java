08da8a4e1d6d661520509033d9f792297feca8b3
hcghcg17 <hcghcg17@naver.com>
2024-04-11 14:06:49 +0900

    Feat : BaekJoon_1431 수정중

---
+++ Algorithm/src/problem_solving/sort/BaekJoon_1431.java
@@ -0,0 +1,25 @@
+package problem_solving.sort;
+
+import java.util.Scanner;
+
+public class BaekJoon_1431 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = Integer.parseInt(sc.next());
+		
+		String [] arr= new String[n];
+		
+		for(int i= 0 ; i < arr.length;i++) {
+			arr[i] = sc.next();
+		}
+		
+		
+		
+		
+	}
+
+}
