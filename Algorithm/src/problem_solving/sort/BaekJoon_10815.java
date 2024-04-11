188f9752ff1a88e2b282bc34866832b4b2552bc3
hcghcg17 <hcghcg17@naver.com>
2024-04-11 12:01:51 +0900

    Feat : BaekJoon_10815 완료

---
+++ Algorithm/src/problem_solving/sort/BaekJoon_10815.java
@@ -0,0 +1,42 @@
+package problem_solving.sort;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class BaekJoon_10815 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = Integer.parseInt(sc.next());
+		
+		
+		Set<Integer> set = new HashSet();
+		
+		for(int i= 0 ; i < n ; i ++) {
+			set.add(Integer.parseInt(sc.next()));
+		}
+		
+		int m = Integer.parseInt(sc.next());
+		
+		int [] arr = new int[m];
+		
+		Arrays.sort(arr);
+		for(int i= 0 ; i < m ; i++) {
+			int num = Integer.parseInt(sc.next());
+			if( set.contains(num)) {
+				arr[i]++;
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int num : arr) {
+			sb.append(num+" ");
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+}
