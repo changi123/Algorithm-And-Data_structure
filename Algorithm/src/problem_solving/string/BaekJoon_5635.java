0c6271bedb17023a919d1888d59345dd6ec75c2a
hcghcg17 <hcghcg17@naver.com>
2024-04-24 14:47:14 +0900

    Feat : BaekJoon_5635 수정중

---
+++ Algorithm/src/problem_solving/string/BaekJoon_5635.java
@@ -0,0 +1,18 @@
+package problem_solving.string;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class BaekJoon_5635 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = Integer.parseInt(sc.next());
+
+	}
+
+}
