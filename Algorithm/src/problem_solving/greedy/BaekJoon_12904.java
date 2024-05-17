7b8041ba5d8c5d41dbe965a10b4191f62beb84ec
hcghcg17 <hcghcg17@naver.com>
2024-05-17 13:23:20 +0900

    Feat : BaekJoon_12904 수정중

---
+++ Algorithm/src/problem_solving/greedy/BaekJoon_12904.java
@@ -0,0 +1,17 @@
+package problem_solving.greedy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJoon_12904 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	
+		
+		
+		
+	}
+
+	
+}
