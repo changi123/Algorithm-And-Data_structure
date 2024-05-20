532d6176f93cc6a89821168de72763160bf15e1e
hcghcg17 <hcghcg17@naver.com>
2024-05-20 14:03:02 +0900

    Feat : BaekJoon_11866 완료

---
+++ Algorithm/src/problem_solving/queue/BaekJoon_11866.java
@@ -0,0 +1,37 @@
+package problem_solving.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BaekJoon_11866 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		int k = Integer.parseInt(sc.next());
+		
+		Queue<Integer> q = new LinkedList();
+		
+		for(int i= 1 ; i<=n;i++ ) {
+			q.offer(i);
+		}
+		StringBuilder sb=  new StringBuilder("<");
+		int i = 1; 
+		while(!q.isEmpty()) {
+			if( i != k) {
+				int num = q.poll();
+				q.offer(num);
+				i++;
+			} else if( q.size()==1) {
+				sb.append(q.poll()+">");
+				break;
+			} else {
+				sb.append(q.poll()+", ");
+				i=1;
+			}
+		}
+		System.out.println(sb.toString());
+	}
+
+}
