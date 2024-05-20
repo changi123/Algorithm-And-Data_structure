ea15898a2c25cff9154ceb63f846b71d533a5f13
hcghcg17 <hcghcg17@naver.com>
2024-05-20 12:39:04 +0900

    Feat : BaekJoon_2164 완료

---
+++ Algorithm/src/problem_solving/queue/BaekJoon_2164.java
@@ -0,0 +1,32 @@
+package problem_solving.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BaekJoon_2164 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Queue<Integer> q = new LinkedList<Integer>();
+
+
+		int n = Integer.parseInt(sc.next());
+		for(int i= 1 ; i <= n ; i ++) {
+			q.offer(i);
+		}
+
+		int i = 1 ;
+		while(q.size()!=1) {
+			q.poll();
+			int num = q.poll();
+			q.add(num);
+			
+		}
+
+
+		System.out.println(q.peek());
+
+	}
+
+}
