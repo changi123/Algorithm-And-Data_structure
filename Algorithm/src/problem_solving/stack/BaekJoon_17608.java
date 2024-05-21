70fce5dedf2f34ad2f61e2a89a5355c29c157309
hcghcg17 <hcghcg17@naver.com>
2024-05-21 14:10:38 +0900

    Feat : BaekJoon_17608 완료

---
+++ Algorithm/src/problem_solving/stack/BaekJoon_17608.java
@@ -0,0 +1,36 @@
+package problem_solving.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BaekJoon_17608 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		Stack<Integer> st = new Stack();
+		for(int i= 0 ; i < n ; i++) {
+			int num = Integer.parseInt(sc.next());
+			st.push(num);
+		}
+		int cnt = 0 ;
+//		System.out.println(st.toString());
+		int last = st.pop();
+		int temp = last ;
+		int i= 0 ;
+		while(!st.isEmpty()) {
+			if(st.peek() > last && st.peek() > temp) {
+				temp = st.peek();
+				st.pop();
+				cnt++;	
+			}else {
+				st.pop();
+			}
+			
+		}
+		
+		System.out.println(cnt+1);
+		
+	}
+
+}
