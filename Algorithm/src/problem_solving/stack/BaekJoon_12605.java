8a94a639949bba08492336ef8ed43cb322184244
hcghcg17 <hcghcg17@naver.com>
2024-05-21 14:18:03 +0900

    Feat : BaekJoon_12605 완료

---
+++ Algorithm/src/problem_solving/stack/BaekJoon_12605.java
@@ -0,0 +1,24 @@
+package problem_solving.stack;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJoon_12605 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		sc.nextLine();
+		
+		for(int i = 0 ; i < n; i ++) {
+			StringBuilder sb= new StringBuilder("Case #"+(i+1)+": ");
+			String s = sc.nextLine();
+			String[] arr = s.split(" ");
+			for(int j = arr.length-1 ;  j >= 0 ; j--) {
+				sb.append(arr[j]+ " ");
+			}
+			System.out.println(sb.toString());
+		}
+	}
+
+}
