28f9fac09950fed327e0508355c4b0638578610c
hcghcg17 <hcghcg17@naver.com>
2024-05-27 11:39:46 +0900

    Feat : BaekJoon_12904

--- Algorithm/src/problem_solving/greedy/BaekJoon_12904.java
+++ Algorithm/src/problem_solving/greedy/BaekJoon_12904.java
@@ -4,14 +4,37 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class BaekJoon_12904 {
+	
+	static StringBuilder sb = new StringBuilder();
+	static StringBuilder makeSb = new StringBuilder();
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-	
-		
-		
+		sb.append(sc.next());
+		makeSb.append(sc.next());
+		while(makeSb.length()>=sb.length()) {
+			
+			if( makeSb.toString().equals(sb.toString())) {
+				System.out.println(1);
+				return ;
+			}
+			
+			if( makeSb.charAt(makeSb.length()-1) == 'A') {
+				makeSb.deleteCharAt(makeSb.length()-1);
+				continue;
+			} else if(makeSb.charAt(makeSb.length()-1) == 'B' ) {
+				makeSb.deleteCharAt(makeSb.length()-1);
+				makeSb.reverse();
+				continue;
+			}
+			
+		}
+			
+			
+		System.out.println(0);
 		
 	}
+	
 
 	
 }
