775dcf15d8193038ac29bcffbef78a2ec8cd9b77
hcghcg17 <hcghcg17@naver.com>
2024-06-14 14:34:06 +0900

    Feat : BaekJoon_2212 완료

---
+++ Algorithm/src/problem_solving/greedy/BaekJoon_2212.java
@@ -0,0 +1,38 @@
+package problem_solving.greedy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJoon_2212 {
+
+	public static void main(String[] args) {
+		Scanner sc=  new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		int k = Integer.parseInt(sc.next());
+		if( k >= n ) {
+			System.out.println(0);
+			return ; 
+		}
+		int [] arr = new int[n];
+		for(int i = 0; i < n ;i++) {
+			arr[i] = Integer.parseInt(sc.next());
+		}
+		
+		Arrays.sort(arr);
+		
+		int [] indexArr = new int[n-1];
+		int index = 0 ; 
+		for(int i= 0 ; i < arr.length -1 ;i++) {
+			indexArr[index++] = arr[i+1]-arr[i];
+		}
+		
+		Arrays.sort(indexArr);
+		int sum = 0 ; 
+		for(int i= 0 ; i < n - k ; i++) {
+			sum+=indexArr[i];
+		}
+		
+		System.out.println(sum);
+	}
+
+}
