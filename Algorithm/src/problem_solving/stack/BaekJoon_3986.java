d4ccc1c7f8da83f4eeca24bc68816b80da972723
hcghcg17 <hcghcg17@naver.com>
2024-06-20 16:06:16 +0900

    Feat : BaekJoon_3986 완료

--- Algorithm/src/problem_solving/stack/BaekJoon_3986.java
+++ Algorithm/src/problem_solving/stack/BaekJoon_3986.java
@@ -9,33 +9,29 @@ public class BaekJoon_3986 {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.next());
 		int cnt = 0 ; 
-		for(int i= 0 ; i < n ; i++) {
+		while(n-- > 0 ) {			
 			Stack<Character> st = new Stack<>();
 			String s = sc.next();
-			char [] arr = s.toCharArray();
-			st.push(arr[0]);
-			for(int j= 1 ; j < arr.length;j++) {
-				if( st.isEmpty()) {
-					st.push(arr[j]);
-					continue;
-				}
-				char c = st.peek();
-				if( arr [j] == c  ) {
-					
-					
-					st.pop();
-				} else if( arr[j] != c && !st.contains(arr[j])) {
-					st.push(arr[j]);
-				} else if( arr[j]!= c && st.contains(arr[j])) {
-					break;
+			char c = s.charAt(0);
+			st.push(c);
+			for(int i= 1 ; i < s.length();i++) {
+				if( !st.isEmpty()) {
+					char stc = st.peek();
+					if( stc == s.charAt(i)) {
+						st.pop();
+					} else {
+						st.push(s.charAt(i));
+					}	
+				}else {					
+					st.push(s.charAt(i));
 				}
 			}
 			if( st.isEmpty()) {
 				cnt++;
 			}
 		}
-
 		System.out.println(cnt);
+			
 	}
 
 }
