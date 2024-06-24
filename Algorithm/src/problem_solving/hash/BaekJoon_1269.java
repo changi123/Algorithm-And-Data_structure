6c1f18eed899c6a53c53c454e4000be58bd5d51d
hcghcg17 <hcghcg17@naver.com>
2024-06-24 11:44:54 +0900

    Feat : BaekJoon_1269 완료

---
+++ Algorithm/src/problem_solving/hash/BaekJoon_1269.java
@@ -0,0 +1,47 @@
+package problem_solving.hash;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class BaekJoon_1269 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		HashSet<Integer> h1 = new HashSet<>();
+		HashSet<Integer> h2 = new HashSet<>();
+		int n = Integer.parseInt(sc.next());
+		int m = Integer.parseInt(sc.next());
+		int answer =0 	 ;
+		for(int i= 0 ; i < n ; i++) {
+			h1.add(Integer.parseInt(sc.next()));
+		}
+		ArrayList<Integer> list1 = new ArrayList<>(h1);
+		for(int i= 0 ; i < m ; i++) {
+			h2.add(Integer.parseInt(sc.next()));
+		}
+		ArrayList<Integer> list2 = new ArrayList<>(h2);
+		
+		for(int i= 0 ; i < list1.size() ; i++) {
+			int num = list1.get(i);
+			if( !h2.contains(num)) {
+				answer++;
+			}
+		}
+		for(int i= 0 ; i < list2.size() ; i++) {
+			int num = list2.get(i);
+			if( !h1.contains(num)) {
+				answer++;
+			}
+		}
+		
+		
+		
+		System.out.println(answer);
+		
+		
+		
+		
+	}
+
+}
