6146c0a0af917c46e769483a0eb816449c993af9
hcghcg17 <hcghcg17@naver.com>
2024-06-24 12:35:56 +0900

    Feat : BaekJoon_2776 완료

---
+++ Algorithm/src/problem_solving/hash/BaekJoon_2776.java
@@ -0,0 +1,40 @@
+package problem_solving.hash;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class BaekJoon_2776 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int T = Integer.parseInt(sc.next());
+		
+		while(T -- > 0) {
+			
+			HashSet<Integer> h = new HashSet<>();
+			int n = Integer.parseInt(sc.next());
+			for(int i= 0 ; i < n ; i++) {
+				h.add(Integer.parseInt(sc.next()));
+			}
+			
+			int m = Integer.parseInt(sc.next());
+			StringBuilder sb = new StringBuilder();
+			for(int i= 0 ; i < m ;i++) {
+				int num = Integer.parseInt(sc.next());
+				if(( h.contains(num))) {
+					sb.append(1);
+				}else {
+					sb.append(0);
+				}
+				
+				if( i == m-1) {
+					break;
+				}
+				sb.append('\n');
+			}
+			System.out.println(sb.toString());
+			
+		}
+	}
+
+}
