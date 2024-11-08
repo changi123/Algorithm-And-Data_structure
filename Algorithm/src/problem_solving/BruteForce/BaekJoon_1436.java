9c59282ab7bb158c9383ead795e16eb556644f44
changi123 <133079671+changi123@users.noreply.github.com>
2024-11-08 13:06:10 +0900

    Feat : BaekJoon_1436 완료

---
+++ Algorithm/src/problem_solving/BruteForce/BaekJoon_1436.java
@@ -0,0 +1,21 @@
+package problem_solving.BruteForce;
+
+import java.util.Scanner;
+
+public class BaekJoon_1436 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		int cnt= 0 ;
+		String start = "666";
+		while(cnt != n ) {
+			if(start.contains("666")) {
+				cnt++;
+			}
+			start = Integer.toString(Integer.parseInt(start)+1) ;
+		}
+		System.out.println(Integer.parseInt(start)-1);
+	}
+
+}
