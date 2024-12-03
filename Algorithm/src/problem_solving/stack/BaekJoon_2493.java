c689db4f2882822377c2abbf659a8473df67ae2c
changi123 <133079671+changi123@users.noreply.github.com>
2024-12-03 16:46:19 +0900

    Feat : BaekJoon_2493 완료

---
+++ Algorithm/src/problem_solving/stack/BaekJoon_2493.java
@@ -0,0 +1,35 @@
+package problem_solving.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BaekJoon_2493 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Stack<int[]> st = new Stack<>();
+		int n = Integer.parseInt(sc.next());
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1 ; i <= n ; i++){
+			int top = Integer.parseInt(sc.next());
+
+			while(!st.isEmpty()) {
+				if( st.peek()[1] >= top ) {
+					sb.append(st.peek()[0]+ " ");
+					break;
+				}
+				
+				st.pop();
+			}
+			
+			if ( st.isEmpty()) {
+				sb.append("0"+ " ");
+			}
+			st.push(new int[] {i,top});
+		}
+		
+		System.out.println(sb.toString());
+
+	}
+
+}
