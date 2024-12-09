510a5ca49e9d38e7c5bc3a499ad2a52fb58d6eae
changi123 <133079671+changi123@users.noreply.github.com>
2024-12-09 20:15:31 +0900

    Feat : BaekJoon_1431 완료

--- Algorithm/src/problem_solving/sort/BaekJoon_1431.java
+++ Algorithm/src/problem_solving/sort/BaekJoon_1431.java
@@ -1,26 +1,56 @@
 package problem_solving.sort;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class BaekJoon_1431 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = Integer.parseInt(sc.next());
-		
+
 		String [] arr= new String[n];
-		
+
 		for(int i= 0 ; i < arr.length;i++) {
 			arr[i] = sc.next();
+		}		
+		String pattern = "[0-9]";
+		Arrays.sort(arr, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				if( o1.length() != o2.length()) {
+					return o1.length() - o2.length();
+				} 
+				int sum1 = 0 ; 
+				for(int i= 0 ; i < o1.length(); i++) {
+					if(   (o1.charAt(i)+"").matches(pattern)) {
+						sum1+=Integer.parseInt(o1.charAt(i)+"");
+					}
+				}
+
+				int sum2 = 0 ;
+				for(int i= 0 ; i < o2.length(); i++) {
+					if(   (o2.charAt(i)+"").matches(pattern)) {
+						sum2+=Integer.parseInt(o2.charAt(i)+"");
+					}
+				}
+
+				if( sum1 != sum2 ) {
+					return sum1 - sum2 ;
+				} 					
+				return o1.compareTo(o2);
+			}
+
+		});
+
+		for(int i= 0 ; i < arr.length; i++) {
+				System.out.println(arr[i]);
 		}
-		
-		Arrays.sort(arr);
-		Arrays.sort(arr, (a1,a2)-> a1.length() );
-		
 	}
 
 }
