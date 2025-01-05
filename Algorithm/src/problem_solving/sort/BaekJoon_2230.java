8ff01fede6cc9f3f2a8a23563f8c1e1f8e133514
changi123 <133079671+changi123@users.noreply.github.com>
2025-01-05 13:52:58 +0900

    Feat : BaekJoon_2230 완료

---
+++ Algorithm/src/problem_solving/sort/BaekJoon_2230.java
@@ -0,0 +1,50 @@
+package problem_solving.sort;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class BaekJoon_2230 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = Integer.parseInt(sc.next());
+		int m = Integer.parseInt(sc.next());
+		
+		int [] arr = new int[n];
+		
+		for(int i = 0 ; i < n; i++) {
+			arr[i] = Integer.parseInt(sc.next());
+		}
+		
+		Arrays.sort(arr);
+		int i = 0 ; 
+		int j = 0 ; 
+		int answer = Integer.MAX_VALUE ; 
+		
+		while(i < n ) {
+			
+			if( arr[i] - arr[j] < m ) {
+				i++;
+				continue;
+			}
+			
+			if( arr[i] - arr[j] == m ) {
+				answer = m ; 
+				break;
+			}
+			
+			answer = Math.min(answer, arr[i]-arr[j]);
+			
+			j++;
+			
+			
+			
+		}
+		System.out.println(answer);
+		
+		
+	}
+
+}
