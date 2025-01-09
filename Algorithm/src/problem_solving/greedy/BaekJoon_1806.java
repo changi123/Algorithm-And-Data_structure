aa4fed3ac044602db98f26c32e92d802dbb756af
changi123 <133079671+changi123@users.noreply.github.com>
2025-01-09 11:54:55 +0900

    Feat : BaekJoon_1806 완료

---
+++ Algorithm/src/problem_solving/greedy/BaekJoon_1806.java
@@ -0,0 +1,43 @@
+package problem_solving.greedy;
+
+import java.util.Scanner;
+
+public class BaekJoon_1806 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = Integer.parseInt(sc.next());
+		int s = Integer.parseInt(sc.next());
+
+
+		int [] arr = new int [n];
+
+		for(int i= 0 ; i < arr.length ; i++) {
+			arr[i] = Integer.parseInt(sc.next());
+		}
+
+		int i = 0 ; 
+		int j = 0 ;
+		int sum = 0 ;
+
+		int len = Integer.MAX_VALUE;
+
+		while(  j < n ) {
+			if (sum < s) {
+				sum += arr[j++];
+			}
+			while (sum >= s) {
+				len = Math.min(j - i, len);
+				sum -= arr[i++];
+			}
+
+		}
+		if( len == Integer.MAX_VALUE) {
+			System.out.println(0);
+		} else {
+			System.out.println(len);
+		}
+	}
+
+}
