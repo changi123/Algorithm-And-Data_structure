ce84170baf5dd076c25ae08188fe4d691ba1c089
changi123 <133079671+changi123@users.noreply.github.com>
2025-01-24 14:53:47 +0900

    Feat : BaekJoon_10870 완료

---
+++ Algorithm/src/problem_solving/math/BaekJoon_10870.java
@@ -0,0 +1,24 @@
+package problem_solving.math;
+
+import java.util.Scanner;
+
+public class BaekJoon_10870 {
+
+
+	public static int Fibo(int num ) {
+
+		if ( num == 0 ) return 0 ;
+		if( num == 1 ) return 1;
+		
+		return Fibo(num -1) + Fibo(num -2);
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc= new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+
+		System.out.println( Fibo(n));
+	}
+
+}
