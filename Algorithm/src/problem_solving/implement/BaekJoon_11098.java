3c0b5cef067a1b8d0b4fd3c08646d9213506452b
changi123 <133079671+changi123@users.noreply.github.com>
2025-02-27 20:09:59 +0900

    Feat : BaekJoon_11098 완료

---
+++ Algorithm/src/problem_solving/implement/BaekJoon_11098.java
@@ -0,0 +1,44 @@
+package problem_solving.implement;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class BaekJoon_11098 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = Integer.parseInt(sc.next());
+		
+		for(int i = 0 ; i < t; i++) {
+			int p = Integer.parseInt(sc.next());
+			String [][] arr = new String[p][2];
+			
+			for(int j = 0 ; j < p ; j++) {
+				
+				arr[j][0] = sc.next();
+				arr[j][1] = sc.next();
+				
+			}
+			
+			Arrays.sort(arr, (a1,a2)->{
+				return Integer.compare(Integer.parseInt(a2[0]), Integer.parseInt(a1[0]));
+			});
+			
+	
+			System.out.println(arr[0][1]);
+		}	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
