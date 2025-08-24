525ac524c8665b54599b50a7b7f190766276465e
changi123 <133079671+changi123@users.noreply.github.com>
2025-08-24 22:55:54 +0900

    Feat : BaekJoon_15700 완료

---
+++ Algorithm/src/problem_solving/implement/BaekJoon_15700.java
@@ -0,0 +1,16 @@
+package problem_solving.implement;
+
+import java.util.Scanner;
+
+public class BaekJoon_15700 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Long n = Long.parseLong(sc.next());
+		Long m = Long.parseLong(sc.next());
+		
+		
+		System.out.print(n * m / 2 );
+	}
+
+}
